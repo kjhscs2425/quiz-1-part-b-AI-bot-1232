8229c5af5732b482675d8ddeae2f219940ca9ced
AI-bot-1232 <nries2026@kehillahstudent.org>
2024-10-23 10:48:10 -0700

    finsished the test

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import turtle
+from turtle import *
+tracer(0,0)
 
 # YOUR CODE HERE
 
-turtle.exitonclick()
+for i in range(4):
+    forward(200)
+    right(90)
+
+forward(100)
+right(45)
+for i in range(4):
+    forward(140)
+    right(90)
+
+
+update()
+exitonclick()
